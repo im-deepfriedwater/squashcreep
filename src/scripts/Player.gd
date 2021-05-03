772194a06890fd73a069db7eff01f3f9dc005d04
jtorre39 <deepfriedwater1@gmail.com>
2021-05-02 17:32:58 -0700

    cam setup

---
+++ src/scripts/Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody
+
+# meters / second
+export var speed = 14
+
+# meters / second squared
+export var fall_acceleration = 75
+
+var velocity = Vector3.ZERO
+
+func _physics_process(delta: float) -> void:
+    var direction = Vector3.ZERO
+    
+    if Input.is_action_pressed('move_right'):
+        direction.x += 1
+    if Input.is_action_pressed('move_left'):
+        direction.x -= 1
+    if Input.is_action_pressed('move_forward'):
+        direction.z -= 1
+    # Notice how we are working with the vector's x and z axes.
+    # In 3D, the XZ plane is the ground plane.
+    if Input.is_action_pressed('move_back'):
+        direction.z += 1
+    
+    if direction != Vector3.ZERO:
+        direction = direction.normalized()
+        $Pivot.look_at(translation + direction, Vector3.UP)
+        
+    # ground velocity
+    velocity.x = direction.x * speed
+    velocity.z = direction.z * speed
+    
+    # vertical velocity
+    velocity.y -= fall_acceleration * delta
+    velocity = move_and_slide(velocity, Vector3.UP)
+        
